189b9d6197d6ed1272f04968bd9451d0792dd464
unknown <mdzuneadabedineidmum@gmail.com>
2024-05-15 23:34:48 +0600

    project structure

---
+++ src/app.ts
@@ -0,0 +1,13 @@
+import { Request, Response } from 'express';
+import cors from 'cors';
+
+import express from 'express';
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+app.get('/', (req: Request, res: Response) => {
+  res.send('Hello World!');
+});
+
+export default app;
